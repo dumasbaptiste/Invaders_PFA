7c29f26f1d2a375858c065c47ef9a11d2968d4b5
baptistedumas <baptiste2d@gmail.com>
2015-06-15 00:42:46 +0200

    clean

--- Invaders_EIP 4.8/Source/Invaders_EIP/Invaders_EIP.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Invaders_EIP.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Invaders_EIP, "Invaders_EIP" );
