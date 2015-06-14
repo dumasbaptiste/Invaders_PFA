01037be91c8378201da68b5bb2096b7f8c80d34a
Ismuur <ismuurpub@yahoo.fr>
2015-06-14 14:54:55 +0200

    Lama

---
+++ Invaders_EIP 4.8/Source/Invaders_EIP/Invaders_EIP.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Invaders_EIP.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Invaders_EIP, "Invaders_EIP" );
